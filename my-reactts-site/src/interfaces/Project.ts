ebbfc181e731a2ff8c16ad75dc509be5bbb54fe4
joaoNovak <joao.gava@mapperidea.io>
2025-03-31 20:58:27 -0300

    feat: correção duplicidade

--- my-reactts-site/src/interfaces/Project.ts
+++
@@ -1,8 +0,0 @@
-// src/interfaces/Project.ts
-export interface Project {
-    id: number;
-    name: string;
-    description: string;
-    status: 'planejamento' | 'em andamento' | 'concluído';
-  }
-  
